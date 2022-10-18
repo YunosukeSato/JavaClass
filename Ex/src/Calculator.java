774aef163892041fcec12fdf351a421f70ac689b
Yunosuke Sato <yunosukesato04@gmail.com>
2022-10-18 00:32:33 -0700

    updated Lab4

--- Ex/src/Calculator.java
+++ Ex/src/Calculator.java
@@ -1,18 +1,80 @@
+// public class Calculator {
+
+//   public static double add(int a, int b) {
+//     return a + b;
+//   }
+
+//   public static double subtract(int a, int b) {
+//     return a - b;
+//   }
+
+//   public static double multiply(int a, int b) {
+//     return a * b;
+//   }
+
+//   public static double divide(int a, int b) {
+//     return a / b;
+//   }
+// }
+
+class InvalidOperatorException extends Exception {
+  public InvalidOperatorException(String str) {
+    super(str);
+  }
+}
+
 public class Calculator {
+  private double num1;
+  private double num2;
+  private double result;
 
-  public static double add(int a, int b) {
-    return a + b;
+  public Calculator() {
+    num1 = 0;
+    num2 = 0;
+    result = 0;
   }
 
-  public static double subtract(int a, int b) {
-    return a - b;
+  public double doCalculation(String op, double num1, double num2) throws InvalidOperatorException, ArithmeticException {
+    this.num1 = num1;
+    this.num2 = num2;
+
+    switch (op) {
+      case "+":
+        result = add();
+        break;
+      case "-":
+        result = subtract();
+        break;
+      case "*":
+        result = multiply();
+        break;
+      case "/":
+        result = divide();
+        break;
+      default:
+        throw new InvalidOperatorException("Invalid operation entered");
+    }
+
+    return result;
   }
 
-  public static double multiply(int a, int b) {
-    return a * b;
+  public double add() {
+    return this.num1 + num2;
   }
 
-  public static double divide(int a, int b) {
-    return a / b;
+  public double subtract() {
+    return this.num1 - num2;
+  }
+
+  public double multiply() {
+    return this.num1 + this.num2;
+  }
+
+  public double divide() {
+      return this.num1 / this.num2;
+  }
+
+  public void printResult(String op) {
+    System.out.println(this.num1 + " " + op + " " + this.num2 + " = " + this.result);
   }
 }
--- Ex/src/Lab4.java
+++ Ex/src/Lab4.java
@@ -1,48 +1,87 @@
 import java.util.Scanner;
 
-public class Lab4 {
-  public static void main(String[] args) {
-    String str = "continue";
+// public class Lab4 {
+//   public static void main(String[] args) {
+//     String str = "continue";
 
-    do {
-      System.out.println("Please type the first number");
-      Scanner scn = new Scanner(System.in);
-      int num1 = scn.nextInt();
+//     do {
+//       System.out.println("Please type the first number");
+//       Scanner scn = new Scanner(System.in);
+//       int num1 = scn.nextInt();
       
-      System.out.println("Choose an operation: " + "\n" +
-      "> Type + for addition" + "\n" + 
-      "> Type - for subtraction" + "\n" +
-      "> Type * for multiplication" + "\n" +
-      "> Type / for division");
-      Scanner scn2 = new Scanner(System.in);
-      String op = scn2.next();
+//       System.out.println("Choose an operation: " + "\n" +
+//       "> Type + for addition" + "\n" + 
+//       "> Type - for subtraction" + "\n" +
+//       "> Type * for multiplication" + "\n" +
+//       "> Type / for division");
+//       Scanner scn2 = new Scanner(System.in);
+//       String op = scn2.next();
 
-      System.out.println("Please type the secont number");
-      Scanner scn3 = new Scanner(System.in);
-      int num2 = scn3.nextInt();
+//       System.out.println("Please type the secont number");
+//       Scanner scn3 = new Scanner(System.in);
+//       int num2 = scn3.nextInt();
 
-      switch (op) {
-      case "+":
-        System.out.println(Calculator.add(num1, num2));
-        break;
-      case "-":
-        System.out.println(Calculator.subtract(num1, num2));
-        break;
-      case "*":
-        System.out.println(Calculator.multiply(num1, num2));
-        break;
-      case "/":
-        if(num1 == 0 || num2 == 0) {
-          System.out.println("Cannot divide by zero");
-        } else {
-          System.out.println(Calculator.divide(num1, num2));
-        }
-        break;
+//       switch (op) {
+//       case "+":
+//         System.out.println(Calculator.add(num1, num2));
+//         break;
+//       case "-":
+//         System.out.println(Calculator.subtract(num1, num2));
+//         break;
+//       case "*":
+//         System.out.println(Calculator.multiply(num1, num2));
+//         break;
+//       case "/":
+//         if(num1 == 0 || num2 == 0) {
+//           System.out.println("Cannot divide by zero");
+//         } else {
+//           System.out.println(Calculator.divide(num1, num2));
+//         }
+//         break;
+//       }
+
+//       System.out.println("Do you want to continue or quit?");
+//       Scanner scn4 = new Scanner(System.in);
+//       str = scn4.next();
+//     } while (str.equals("continue"));
+//   }
+// }
+
+public class Lab4 {
+  public static void main(String[] args) throws Exception {
+    Calculator calc = new Calculator();
+    Scanner scanner =  new Scanner(System.in);
+    String exit = "";
+
+    while(!exit.toLowerCase().equals("quit")) {
+      try {
+        System.out.println("Please type the first number: ");
+        double n1 = scanner.nextDouble();
+        System.out.println("Choose an operation:\n"
+          + " > Type + for addition\n"
+          + " > Type - for subtraction\n"
+          + " > Type * for multiplication\n"
+          + " > Tupe / for division\n");
+          String op = scanner.next();
+          System.out.println("Type the second number:");
+          double n2 = scanner.nextDouble();
+
+          calc.doCalculation(op, n1, n2);
+          calc.printResult(op);
+
+          System.out.println("Do you want to 'continue' or 'quit'?");
+
+          exit = scanner.next();
+      } catch (InvalidOperatorException e) {
+        System.out.println(e.getMessage());
+      } catch (ArithmeticException e) {
+        System.out.println("Cannot divide by zero. Please try again...");
       }
 
-      System.out.println("Do you want to continue or quit?");
-      Scanner scn4 = new Scanner(System.in);
-      str = scn4.next();
-    } while (str.equals("continue"));
+    }
+
+    System.out.println("Bye");
+    scanner.close();
+    System.exit(0);
   }
 }
